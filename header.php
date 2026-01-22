55422249bce6f899003d4a0d4e83bf164af1cbcb
アウンミョウライン_SUNSEER_東京 <24jn0201@jec.ac.jp>
2026-01-23 01:51:09 +0900

    Final fix: cleanup header and remove header1

--- header.php
+++ header.php
@@ -1,11 +1,16 @@
 <?php
     require_once './helpers/MemberDAO.php';
+
     if(session_status() === PHP_SESSION_NONE){
         session_start();
     }
-    $member = isset($_SESSION['member']) ? $_SESSION['member'] : null;
+    
+    $member = null;
+    if(isset($_SESSION['member'])){
+        $member = $_SESSION['member'];
+    }
 ?>
-<nav class="navbar navbar-expand-lg navbar-white bg-white">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="top.php">
             <img src="images/ロゴ.png" alt="ロゴ" height="50">
@@ -15,6 +20,8 @@
                 <?php if(isset($member)) : ?>
                     <li class="nav-item"><a href="cart.php" class="nav-link" style="color: black;">カート</a></li>
                     <li class="nav-item"><a href="mypage.php" class="nav-link" style="color: black;">マイページ</a></li>
+                    <li class="nav-item"><a href="okiniiri.php" class="nav-link" style="color: black;">お気に入り</a></li>
+                    <li class="nav-item"><a href="logout.php" class="nav-link" style="color: black;">ログアウト</a></li>
                 <?php else: ?>
                     <li class="nav-item"><a href="login.php" class="nav-link" style="color: black;">ログイン</a></li>
                 <?php endif; ?>
